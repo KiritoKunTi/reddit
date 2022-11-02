103c279de050d7e64ec1139e64af4712691b2c58
duman <duman200354@mail.ru>
2022-11-02 23:25:45 +0600

    signup styled

---
+++ src/components/HOR.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const HOR = () => {
+  return (
+    <div className="w-full flex justify-between items-center my-10">
+      <div className="w-[40%] h-[0.5px] bg-gray-300"></div>
+      <p className="font-bold text-lg text-gray-500">OR</p>
+      <div className="w-[40%] h-[1px] bg-gray-300"></div>
+    </div>
+  );
+};
+
+export default HOR;
--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -2,14 +2,16 @@ import React, { useState } from "react";
 import { auth } from "../firebase";
 import { SignUpWithPopup, GoogleAuthProvider } from "firebase/auth";
 import GoogleButton from "./GoogleButton";
+import HOR from "./HOR";
 
 const style = {
-  button: `text-xl font-medium min-w-[160px] px-6 py-2 border rounded-full text-white  hover:bg-zinc-900 mr-6`,
+  button: `bg-[#d93a00] text-white font-medium text-[20px] py-3 rounded-full hover:bg-[#d82a00]`,
   container: `absolute bg-white w-[500px] h-[83vh] rounded-3xl p-20 top-[50%] left-[50%] transform translate-x-[-50%] translate-y-[-50%] `,
   popup: `flex flex-col justify-center w-full h-full`,
   heading: `text-2xl font-medium mb-2`,
   subtitle: `mb-12`,
   link: `text-blue-700`,
+  emailInput: `px-5 py-4 border hover:border-gray-400 rounded-full text-lg font-medium bg-gray-100 mb-6 active:border-blue-700 outline-none`,
 };
 
 const SignUp = () => {
@@ -36,6 +38,13 @@ const SignUp = () => {
         </p>
 
         <GoogleButton />
+        <HOR />
+        <input className={style.emailInput} placeholder="Email" />
+        <button className={style.button}>Continue</button>
+        <p className="mt-5">
+          Already a redditor?{" "}
+          <span className="text-blue-600 underline-offset-1">Log In</span>
+        </p>
       </div>
     </div>
   );
